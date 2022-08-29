46cc24356d3e680dfaa94369c7bfbc30a7d763e6
swatikadam-123 <swatikadam907@gmail.com>
2022-08-29 20:25:58 +0530

    added : case que-2  read num display week in word

---
+++ case que-2.sh
@@ -0,0 +1,33 @@
+read -p "enter  week number :"  week
+
+
+case $week in
+ 
+1) echo  "sunday"
+ 
+   ;;
+ 
+2) echo  "monday"
+   
+ ;;
+ 
+3) echo  "tuesday"
+   
+ ;;
+ 
+4) echo  "wednesday"
+   
+ ;;
+ 
+5) echo  "turday"
+  
+  ;;
+ 
+6) echo  "firday"
+  
+  ;;
+ 
+7) echo  "saturday"
+   
+ ;;
+esac
